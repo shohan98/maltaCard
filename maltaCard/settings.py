db89622a8f41b52e90e88401f327fce024421add
@mahbub.alam <mahbub.alam@technonext.com>
2025-08-09 18:12:42 +0600

    api cross issue fixed and update settings.py file

--- maltaCard/settings.py
+++ maltaCard/settings.py
@@ -51,11 +51,13 @@ INSTALLED_APPS = [
     'authentication',
     'card_management',
     'social_django',  # For Google OAuth
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'core.middleware.CSRFExemptMiddleware',  # Exempt API endpoints from CSRF
@@ -93,6 +95,11 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'maltaCard.wsgi.application'
+
+# React development server
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  
+]
 
 
 # Database
